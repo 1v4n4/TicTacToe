3ecbcb706d845f82ab1d263edd0f0ab7f5b70925
Victor Otieno <victorotieno598@gmail.com>
2021-05-06 17:54:56 +0300

    Add won method

--- bin/main.rb
+++ bin/main.rb
@@ -3,6 +3,12 @@
 board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 players = []
 
+WINNERS = [ 
+    [0, 1, 2], [3, 4, 5], [6, 7, 8], 
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],
+    [6, 4, 2], [0, 4, 8] 
+]
+
 def display_board(board)
   puts '+---+---+---+'
   puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
@@ -40,17 +46,28 @@ players << { 'name' => player_two, 'sym' => 'Y' }
 
 puts "#{player_one} will use 'X while #{player_two} will use 'Y'"
 
+def won?(board)
+  (board[0] == board[1] && board[1] == board[2]) ||
+  (board[3] == board[4] && board[4] == board[5]) ||
+  (board[6] == board[7] && board[7] == board[8]) ||
+  (board[0] == board[3] && board[3] == board[6]) ||
+  (board[1] == board[4] && board[4] == board[7]) ||
+  (board[2] == board[5] && board[5] == board[8]) ||
+  (board[0] == board[4] && board[4] == board[8]) ||
+  (board[2] == board[4] && board[4] == board[6])
+end
+
 def clear_screen(arr)
   system 'clear'
   system 'cls'
   display_board(arr)
 end
 
-def tie(arr)
+def tie?(arr)
  arr.none? {|a| a.is_a?(Integer) }
 end
 
- while !tie(board) do
+while !won?(board) || !tie?(board) do
 
 puts "#{player_one} it's your move"
 puts 'Please select an available cell from the board'
@@ -63,7 +80,7 @@ board[answer - 1] = 'X'
 
 clear_screen(board)
 
- break if tie(board)
+break if won?(board) || tie?(board) 
 
 puts "#{player_two} it's your move"
 puts 'Please select an available cell from the board'
@@ -74,6 +91,8 @@ until board.include?(answer)
 end
 board[answer - 1] = 'O'
 
+break if won?(board) || tie?(board) 
+
 clear_screen(board)
 
 end
@@ -83,6 +102,5 @@ puts "#{player_one} won the game"
 # elsif won && answer == 'O'
 puts "#{player_two} won the game"
 # else
-puts "It's a tie\n Game over!"
+puts "It's a tie?\n Game over!"
 # end
-
