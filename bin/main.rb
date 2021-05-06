951bb1743faa83027cd9d2bf9683c600c61c075e
Victor Otieno <victorotieno598@gmail.com>
2021-05-06 11:19:47 +0300

    Add diplay board method

--- bin/main.rb
+++ bin/main.rb
@@ -1,3 +1,28 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World'
+board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+players = []
+
+def display_board(board)
+   puts "+---+---+---+"
+   puts "| #{board[0]} | #{board[1]} | #{board[2]} |"
+   puts "+---+---+---+"
+   puts "| #{board[3]} | #{board[4]} | #{board[5]} |"
+   puts "+---+---+---+"
+   puts "| #{board[6]} | #{board[7]} | #{board[8]} |"
+   puts "+---+---+---+"
+end
+
+display_board(board)
+
+print 'Enter Player 1 name: '
+player_one = gets.strip.chomp.capitalize
+players << {'name'=>player_one, 'sym'=>'X'}
+
+print 'Enter Player 2 name: '
+player_two = gets.strip.chomp.capitalize
+players << {'name'=>player_two, 'sym'=>'Y'}
+
+puts "#{player_one} will use 'X while #{player_two} will use 'Y'"
+
+p players
