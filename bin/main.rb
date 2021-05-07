16b10ef836f212ea9493b2f1cb45c5146e0bfab5
ivana <ivananl@yahoo.com>
2021-05-07 19:24:39 +0200

    Add changes code reviewer requested

--- bin/main.rb
+++ bin/main.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
-require './lib/game_logic'
-require './lib/helpers'
-require './lib/player'
+require_relative '../lib/game_logic'
+require_relative '../lib/helpers'
+require_relative '../lib/player'
 
 # rubocop: disable Style/MixinUsage
 include TicTacToe
@@ -11,31 +11,36 @@ include TicTacToe
 count = 0
 
 new_board = TicTacToe::Board.new
-new_board.display_board
+puts new_board.display_board
 
 puts "Welcome to Victor\'s and Ivana\'s Tic Toc"
 puts 'Let the game begin!'
 print 'Player 1, enter your name: '
 name = gets.strip.chomp.capitalize
 
-check_name(name)
+while name.empty?
+  puts empty_msg
+  name = gets.strip.chomp.capitalize
+end
 
 player1 = TicTacToe::Player.new(name, 'X')
 
 print 'Player 2, enter your name: '
 name = gets.strip.chomp.capitalize
-check_name(name)
+
+while name.empty?
+  puts empty_msg
+  name = gets.strip.chomp.capitalize
+end
 
 player2 = TicTacToe::Player.new(name, 'O')
 
 puts "#{player1.name} will use #{player1.sym} while #{player2.name} will use #{player2.sym}"
 
-new_board.clear_screen
-
-# rubocop: disable Lint/Void
 # rubocop: disable Style/IdenticalConditionalBranches
 
-clear = new_board.clear_screen
+clear_screen
+puts new_board.display_board
 
 while !new_board.won? || !new_board.tie?
 
@@ -44,13 +49,14 @@ while !new_board.won? || !new_board.tie?
   answer = gets.chomp.to_i
 
   until new_board.board.include?(answer)
-    invalid
+    puts invalid
     answer = gets.chomp.to_i
   end
   new_board.board[answer - 1] = 'X'
   count += 1
 
-  new_board.clear_screen
+  clear_screen
+  puts new_board.display_board
 
   break if new_board.won? || new_board.tie?
 
@@ -59,25 +65,28 @@ while !new_board.won? || !new_board.tie?
   answer = gets.chomp.to_i
 
   until new_board.board.include?(answer)
-    invalid
+    puts invalid
     answer = gets.chomp.to_i
   end
   new_board.board[answer - 1] = 'O'
   count += 1
   break if new_board.won? || new_board.tie?
 
-  new_board.clear_screen
+  clear_screen
+  puts new_board.display_board
 end
 
 if new_board.won? && count.odd?
-  clear
+  clear_screen
+  puts new_board.display_board
   puts "#{player1.name} won the game!"
 elsif new_board.won? && count.even?
-  clear
+  clear_screen
+  puts new_board.display_board
   puts "#{player2.name} won the game!"
 else
-  clear
-  tie_msg
+  clear_screen
+  puts new_board.display_board
+  puts tie_msg
 end
-# rubocop: enable Lint/Void
 # rubocop: enable Style/IdenticalConditionalBranches
