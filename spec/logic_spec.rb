ca1652e2b7a47c3cdcaee877ba52b021e5580bb9
ivana <ivananl@yahoo.com>
2021-05-12 15:36:29 +0200

    Add tests for helpeps

--- spec/logic_spec.rb
+++ spec/logic_spec.rb
@@ -8,6 +8,9 @@ describe Board do
     it 'creates an array of numbers from 1 to 9' do
       expect(subject.board).to eql([1, 2, 3, 4, 5, 6, 7, 8, 9])
     end
+
+    it 'creates a counter with the value of zero'
+     expect(subject.counter).to eql(0)
   end
 
   describe '#display_board' do
