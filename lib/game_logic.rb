2a584a33d50cb4db167a9aee5c7824d37ad19dcd
Victor Otieno <victorotieno598@gmail.com>
2021-05-18 17:37:56 +0300

    Fix update_board method in game_logic

--- lib/game_logic.rb
+++ lib/game_logic.rb
@@ -4,6 +4,7 @@ module TicTacToe
   class Board
     def initialize
       @board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
       @counter = 0
     end
 
@@ -12,32 +13,43 @@ module TicTacToe
     end
 
     def update_board(answer)
-      @board[answer - 1] = if @counter.odd?
-                             'X'
-                           else
-                             'O'
-                           end
+      @board[answer - 1] = @counter.odd? ? 'X' : 'O'
     end
 
     def display_board
       ['+---+---+---+',
+
        "| #{@board[0]} | #{@board[1]} | #{@board[2]} |",
+
        '+---+---+---+',
+
        "| #{@board[3]} | #{@board[4]} | #{@board[5]} |",
+
        '+---+---+---+',
+
        "| #{@board[6]} | #{@board[7]} | #{@board[8]} |",
+
        '+---+---+---+']
     end
 
     WINNERS = [
+
       [0, 1, 2], # 1st horizontal row
+
       [3, 4, 5], # 2nd horizontal row
+
       [6, 7, 8], # 3rd horizontal row
+
       [0, 3, 6], # 1st vertial column
+
       [1, 4, 7], # 2nd vertial column
+
       [2, 5, 8], # 3rd vertial column
+
       [6, 4, 2], # right diagonal
+
       [0, 4, 8] # left diagonal
+
     ].freeze
 
     def won?
