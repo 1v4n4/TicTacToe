369f559241add8970991505de983c17304f5e3ef
Victor Otieno <victorotieno598@gmail.com>
2021-05-18 12:36:43 +0300

    Add helpers_spec file to test helper methods

---
+++ spec/helpers_spec.rb
@@ -0,0 +1,20 @@
+require './lib/helpers'
+include TicTacToe
+
+describe '#invalid' do
+    it "returns invalid move message" do
+        expect(invalid).to eql('Invalid move. Please enter a number of an available cell')
+    end
+end
+
+describe '#tie_msg' do
+    it "returns tie message when the game is over and there is no winner" do
+        expect(tie_msg).to eql("It's a tie\n Game over!")
+    end
+end
+
+describe '#empty_msg' do
+    it "returns warning message when user has entered a blank space" do
+        expect(empty_msg).to eql("You can't leave this empty. Please, enter you name")
+    end
+end
